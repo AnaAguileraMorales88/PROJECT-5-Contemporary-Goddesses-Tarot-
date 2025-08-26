882dcf3e6baaa02578a2ff40486666b5f86e118a
Luisa Moreno <lumoreno323@gmail.com>
2025-08-26 09:26:09 +0200

    Se realiza la instalación de REACT y de Tailwind, se prueba su funcionamiento con un ejemplo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen grid place-items-center bg-rose-600">
+      <h1 className="text-4xl font-bold text-black">React + Tailwind ✅</h1>
+    </div>
+  );
+}
+export default App;

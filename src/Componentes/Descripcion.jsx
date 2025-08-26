8bc0ba2f4898294a3bc522b0f2594dcf1907cfb7
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2025-08-26 10:00:17 +0200

    Añado header y descripcion

---
+++ src/Componentes/Descripcion.jsx
@@ -0,0 +1,11 @@
+const Descripcion = () => {
+    return(
+            <section>
+      <h2 className="text-center text-base sm:text-lg md:text-xl lg:text-2xl mt-6 mb-6 px-4 sm:px-6 md:px-12">
+        Un tarot virtual que te invita a descubrir los secretos de tu pasado, comprender las señales de tu presente y vislumbrar los caminos de tu futuro. Elige tus cartas y deja que hablen por ti, revelando mensajes ocultos que esperan ser descubiertos.
+      </h2>
+    </section>
+    )
+}
+
+export default Descripcion;
---
+++ src/Componentes/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+<header>
+  <h1 className="text-center text-2xl sm:text-3xl md:text-5xl lg:text-7xl font-bold mt-12 mb-8">
+    DIOSAS MÍSTICAS
+  </h1>
+
+</header>
+    )
+}
+
+export default Header;

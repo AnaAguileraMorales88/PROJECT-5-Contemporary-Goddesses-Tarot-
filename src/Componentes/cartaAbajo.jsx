fdd7d5a060ad8eb98db840dada2b8702ad577484
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2025-08-27 14:37:57 +0200

    Cambios en el diseño y  añadidas imagenes de fondo

--- src/Componentes/cartaAbajo.jsx
+++ src/Componentes/cartaAbajo.jsx
@@ -5,39 +5,39 @@ const CartaAbajo = ({ datosUsuario }) => {
     <section className="flex flex-col items-center justify-center mt-10 px-4">
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-20 text-center">
         <div>
-          <h3 className="text-[#D3A85D] font-[Lugrasimo] text-2xl sm:text-3xl md:text-3xl mb-3">
+          <h3 className="text-[#FDDBA1] font-[Lugrasimo] text-2xl sm:text-3xl md:text-3xl mb-3">
            Pasado
           </h3>
           <img
             src={CartaTarot}
             alt="Carta de Tarot boca abajo - Pasado"
-           className="w-40 sm:w-48 md:w-56 lg:w-64 shadow-2xl shadow-black/60 rounded-lg cursor-pointer hover:scale-103 transition-transform duration-300 border-2 border-[#D3A85D]"
+           className="w-40 sm:w-48 md:w-56 lg:w-64 shadow-2xl shadow-black/60 rounded-lg cursor-pointer hover:scale-103 transition-transform duration-300 border-10 border-[#FDDBA1]"
           />
         </div>
 
         <div>
-          <h3  className="text-[#D3A85D] font-[Lugrasimo] text-2xl sm:text-3xl md:text-3xl mb-3">Presente</h3>
+          <h3  className="text-[#FDDBA1] font-[Lugrasimo] text-2xl sm:text-3xl md:text-3xl mb-3">Presente</h3>
           <img
             src={CartaTarot}
             alt="Carta de Tarot boca abajo - Presente"
-           className="w-40 sm:w-48 md:w-56 lg:w-64 shadow-2xl shadow-black/60 rounded-lg cursor-pointer hover:scale-103 transition-transform duration-300 border-2 border-[#D3A85D]"
+           className="w-40 sm:w-48 md:w-56 lg:w-64 shadow-2xl shadow-black/60 rounded-lg cursor-pointer hover:scale-103 transition-transform duration-300 border-10 border-[#FDDBA1]"
           />
         </div>
 
         <div>
-          <h3  className="text-[#D3A85D] font-[Lugrasimo] text-2xl sm:text-3xl md:text-3xl mb-3">Futuro</h3>
+          <h3  className="text-[#FDDBA1] font-[Lugrasimo] text-2xl sm:text-3xl md:text-3xl mb-3">Futuro</h3>
           <img
             src={CartaTarot}
             alt="Carta de Tarot boca abajo - Futuro"
-           className="w-40 sm:w-48 md:w-56 lg:w-64 shadow-2xl shadow-black/60 rounded-lg cursor-pointer hover:scale-103 transition-transform duration-300 border-2 border-[#D3A85D]"
+           className="w-40 sm:w-48 md:w-56 lg:w-64 shadow-2xl shadow-black/60 rounded-lg cursor-pointer hover:scale-103 transition-transform duration-300 border-10 border-[#FDDBA1]"
           />
         </div>
       </div>
 
       {datosUsuario && (
         <section className="text-center mt-10">
-          <h3 className="text-xl text-[#D3A85D] sm:text-4xl font-bold mb-4 mt-8">
-            ¡ Bienvenid@, {datosUsuario.nombre} !
+          <h3 className="text-xl text-[#FDDBA1] sm:text-4xl font-bold mb-4 mt-8">
+            ¡ {datosUsuario.nombre} !
           </h3>
           <p className="text-white text-base sm:text-lg md:text-xl lg:text-1xl font-bold mb-4">
          Tu última tirada fue registrada el <strong>{datosUsuario.fecha}</strong>
--- src/Componentes/inputUsuario.jsx
+++ src/Componentes/inputUsuario.jsx
@@ -20,12 +20,12 @@ function Usuario({ onRegistro }) {
   };
 
   return (
-    <section className="p-6 max-w-md mx-auto rounded-2xl shadow-md">
+    <section className="p-6 max-w-md mx-auto rounded-2xl shadow-md shadow-white">
       <form onSubmit={manejarEnvio} className="space-y-6 text-center">
         <div className="flex flex-col sm:flex-row items-center justify-center sm:space-x-4 space-y-4 sm:space-y-0">
           <label
             htmlFor="nombre"
-            className="text-xl sm:text-2xl font-semibold text-white "
+            className="text-xl sm:text-2xl font-semibold text-white mt-6"
           >
             Nombre:
           </label>
@@ -34,14 +34,14 @@ function Usuario({ onRegistro }) {
             type="text"
             value={nombre}
             onChange={(e) => setNombre(e.target.value)}
-            className="p-1 w-40 sm:w-48 text-center bg-white text-gray-600 placeholder-gray-400"
+            className="p-2 w-40 sm:w-48 text-center rounded-lg  bg-white text-black-600 placeholder-black mt-6"
             placeholder="Escribe tu nombre *"
             required
           />
         </div>
         <button
           type="submit"
-          className="bg-[#C99AE9] text-[#040813] px-6 py-2 rounded-lg cursor-pointer hover:bg-[#D3A85D] mt-6 mb-8 text-xl"
+          className="bg-[#C99AE9] text-[#040813] px-6 py-2 rounded-lg cursor-pointer hover:bg-[#FDDBA1] mt-6 mb-6 text-xl"
         >
           Guardar
         </button>

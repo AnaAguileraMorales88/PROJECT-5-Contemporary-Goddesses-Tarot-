6d2028ddea2803f4ef4acda29f7293d1fccbb57a
Andrea <aoliverar.96@gmail.com>
2025-09-01 09:58:55 +0200

    Ruta arreglada

--- src/Components/cards.jsx
+++ src/Components/cards.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState, useRef } from "react";
 import TarotCard from "../assets/images/Carta_Tarot.jpg";
 import { getCards } from "../services/ApiCards";
 import SelectedCards from "./selectedCards";
-import CardsResult from "./cardsResult";
+import CardsResult from "./CardsResult";
 import AlertPopup from "./modals/popupAlert";
 import StartModal from "./modals/startModal";
 import EndModal from "./modals/endModal";

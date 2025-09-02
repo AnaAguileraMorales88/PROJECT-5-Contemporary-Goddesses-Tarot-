0529625720b8f670b3ca8a12b7034f844f772100
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2025-09-02 13:16:41 +0200

    Añadido el boton de vaciar historial y funcional/formato al documento

--- src/Components/clearHistory.jsx
+++ src/Components/clearHistory.jsx
@@ -1,19 +1,19 @@
 const ClearHistory = ({ onClear }) => {
-  return (
-    <button
-      onClick={onClear}
-className="
-  absolute right-2 md:right-0 top-1/2 transform -translate-y-1/2
-  bg-[#392F52] hover:bg-red-700 text-white
-  text-[10px] sm:text-xs md:text-base
-  px-2 py-1 sm:px-2 sm:py-1 md:px-3 md:py-1.5
-  rounded-lg shadow-md transition cursor-pointer
+    return (
+        <button
+            onClick={onClear}
+            className="
+absolute right-2 md:right-0 top-1/2 transform -translate-y-1/2
+bg-[#392F52] hover:bg-red-700 text-white
+text-[10px] sm:text-xs md:text-base
+px-2 py-1 sm:px-2 sm:py-1 md:px-3 md:py-1.5
+rounded-lg shadow-md transition cursor-pointer
 "
 
-    >
-      Eliminar historial
-    </button>
-  );
+        >
+            Eliminar historial
+        </button>
+    );
 };
 
 export default ClearHistory;
